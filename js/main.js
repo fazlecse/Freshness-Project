050a08f33e35fe751c7676c708aba56ec12a356a
Md. Fazle Rabbi <78721987+fazlenub@users.noreply.github.com>
2021-10-09 17:28:18 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+    $(document).ready(function(){
+      $('.slider').bxSlider();
+      $('#menu').slicknav();
+    });
+
+  // jqurry bx-slider//
+$(document).ready(function(){
+    $('.slider').bxSlider();
+  });
+
+  // slicknav//
+  $(document).ready(function(){
+    $('#menu').slicknav();
+  });
